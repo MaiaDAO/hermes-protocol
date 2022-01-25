92dad0c42bb952b360d6497dfac55ef7b817e573
Andre Cronje <andre.cronje@yearn.finance>
2022-01-25 05:16:01 +0200

    Audit: PVE008 -2
    Signed-off-by: Andre Cronje <andre.cronje@yearn.finance>

--- contracts/BaseV1-core.sol
+++ contracts/BaseV1-core.sol
@@ -77,7 +77,7 @@ contract BaseV1Pair {
     mapping(address => mapping (address => uint)) public allowance;
     mapping(address => uint) public balanceOf;
 
-    bytes32 immutable DOMAIN_SEPARATOR;
+    bytes32 DOMAIN_SEPARATOR;
     // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
     bytes32 constant PERMIT_TYPEHASH = 0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
     mapping(address => uint) public nonces;
@@ -140,17 +140,6 @@ contract BaseV1Pair {
     event Sync(uint reserve0, uint reserve1);
 
     constructor() {
-        uint chainId = block.chainid;
-        DOMAIN_SEPARATOR = keccak256(
-            abi.encode(
-                keccak256('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)'),
-                keccak256(bytes(name)),
-                keccak256(bytes('1')),
-                chainId,
-                address(this)
-            )
-        );
-
         (address _token0, address _token1, bool _stable) = BaseV1Factory(msg.sender).getInitializable();
         (token0, token1, stable) = (_token0, _token1, _stable);
         fees = address(new BaseV1Fees(_token0, _token1));
@@ -520,6 +509,15 @@ contract BaseV1Pair {
 
     function permit(address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external {
         require(deadline >= block.timestamp, 'BaseV1: EXPIRED');
+        DOMAIN_SEPARATOR = keccak256(
+            abi.encode(
+                keccak256('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)'),
+                keccak256(bytes(name)),
+                keccak256(bytes('1')),
+                block.chainid,
+                address(this)
+            )
+        );
         bytes32 digest = keccak256(
             abi.encodePacked(
                 '\x19\x01',
