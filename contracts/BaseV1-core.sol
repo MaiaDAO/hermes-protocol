ab72cf1185a96534d55329a1b1d42bbceb535bfc
Andre Cronje <andre.cronje@yearn.finance>
2022-01-29 08:49:00 +0200

    feat: additional code.length checks on all _safe* functions, because paranoia
    Signed-off-by: Andre Cronje <andre.cronje@yearn.finance>

--- contracts/BaseV1-core.sol
+++ contracts/BaseV1-core.sol
@@ -574,12 +574,14 @@ contract BaseV1Pair {
     }
 
     function _safeTransfer(address token,address to,uint256 value) internal {
+        require(token.code.length > 0);
         (bool success, bytes memory data) =
             token.call(abi.encodeWithSelector(erc20.transfer.selector, to, value));
         require(success && (data.length == 0 || abi.decode(data, (bool))));
     }
 
     function _safeTransferFrom(address token, address from, address to, uint256 value) internal {
+        require(token.code.length > 0);
         (bool success, bytes memory data) =
             token.call(abi.encodeWithSelector(erc20.transferFrom.selector, from, to, value));
         require(success && (data.length == 0 || abi.decode(data, (bool))));
--- contracts/BaseV1-gauges.sol
+++ contracts/BaseV1-gauges.sol
@@ -550,18 +550,21 @@ contract Gauge {
     }
 
     function _safeTransfer(address token, address to, uint256 value) internal {
+        require(token.code.length > 0);
         (bool success, bytes memory data) =
             token.call(abi.encodeWithSelector(erc20.transfer.selector, to, value));
         require(success && (data.length == 0 || abi.decode(data, (bool))));
     }
 
     function _safeTransferFrom(address token, address from, address to, uint256 value) internal {
+        require(token.code.length > 0);
         (bool success, bytes memory data) =
             token.call(abi.encodeWithSelector(erc20.transferFrom.selector, from, to, value));
         require(success && (data.length == 0 || abi.decode(data, (bool))));
     }
 
     function _safeApprove(address token, address spender, uint256 value) internal {
+        require(token.code.length > 0);
         (bool success, bytes memory data) =
             token.call(abi.encodeWithSelector(erc20.approve.selector, spender, value));
         require(success && (data.length == 0 || abi.decode(data, (bool))));
