d41e2a23679727e1d23e57d73a0d514dec1d23b7
Andre Cronje <andre.cronje@yearn.finance>
2022-02-05 13:39:01 +0200

    slither: dead-code
    Signed-off-by: Andre Cronje <andre.cronje@yearn.finance>

--- contracts/BaseV1-voter.sol
+++ contracts/BaseV1-voter.sol
@@ -2,9 +2,6 @@
 pragma solidity 0.8.11;
 
 library Math {
-    function max(uint a, uint b) internal pure returns (uint) {
-        return a >= b ? a : b;
-    }
     function min(uint a, uint b) internal pure returns (uint) {
         return a < b ? a : b;
     }
